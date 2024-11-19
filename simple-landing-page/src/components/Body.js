06f254cc9860401d2e4de46c35ee2a80e7275eb8
HolubYuu <yuliia.holub.pz.2023@lpnu.ua>
2024-11-19 15:50:35 +0200

    Holud_Body.js

--- simple-landing-page/src/components/Body.js
+++ simple-landing-page/src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Yuli.jpg"} alt="Yulia Holub" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Yulia Holub</h2>
+          <p style={styles.position}>QA</p>
         </div>
       </div>
     </main>
