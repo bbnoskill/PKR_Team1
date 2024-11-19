5fc922d6ba15fd68116208388c5e1c79e68a3ba9
bVitalij <189000450+bVitalij@users.noreply.github.com>
2024-11-19 15:49:58 +0200

    Update Body.js

--- simple-landing-page/src/components/Body.js
+++ simple-landing-page/src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-default.png"} alt="Vitalij Bahurskij" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Vitalij Bahurskij</h2>
+          <p style={styles.position}>Developer</p>
         </div>
       </div>
     </main>
