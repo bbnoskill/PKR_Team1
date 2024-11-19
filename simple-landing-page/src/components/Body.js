38a0dd52aef6fb16dc5478bd9bf919ff95e7a4f1
Евеліна <evelina.bashynska.pz.2023@lpnu.ua>
2024-11-19 15:39:36 +0200

    Update Body.js

--- simple-landing-page/src/components/Body.js
+++ simple-landing-page/src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-default.png"} alt="Bashynska Evelina" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Bashynska Evelina</h2>
+          <p style={styles.position}>Backend engineer</p>
         </div>
       </div>
     </main>
